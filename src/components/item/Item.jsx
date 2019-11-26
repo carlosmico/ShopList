bcdd8dcf4f66564fc25dd2c7976ea5478daf42d2
Carlos Mico <carlosmico15@gmail.com>
2019-11-26 20:24:24 +0100

    Design changes

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -23,41 +23,19 @@ class Item extends React.Component {
         })
     }
 
-    // decrement = (event) => {
-    //     if(this.state.units > 1){
-    //         this.setState({
-    //             units: this.state.units - 1
-    //         }, ()=>{
-    //             this.props.updateItem(this.state);
-    //         });
-    //     }
-    // }
-
-    // increment = (event) => {
-    //     this.setState({
-    //         units: this.state.units + 1
-    //     }, ()=>{
-    //         this.props.updateItem(this.state);
-    //     });
-        
-    // }
-
     render() {
         return (
             <div className='item' id={this.props.id}>
                 <div>
-                    <input className='.inputItem' contentEditable="true" onBlur={event=>this.props.update(event, this.state)} onChange={this.handleChange} value={this.state.title}/>
+                    <input className='.inputItem' contentEditable="true" onBlur={event => this.props.update(event, this.state)} onChange={this.handleChange} value={this.state.title} />
 
                     <div className='buttoner'>
-                        <i className="fas fa-trash-alt removeBtn" onClick={event=>this.props.remove(event, this.state)}></i>
+                        {this.props.checkedItem ?
+                            <i className="fas fa-trash-alt removeBtn" onClick={event => this.props.removeOrChecked(event, this.state)}></i> :
+                            ""
+                        }
                     </div>
                 </div>
-
-                {/* <div>
-                    <a className="customBtn" onClick={this.decrement}><span>-</span></a>
-                    <span className="units">{this.state.units}</span>
-                    <a className="customBtn" onClick={this.increment}><span>+</span></a>
-                </div> */}
             </div>
         );
     }
